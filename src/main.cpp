2521b404b1d3da5051b3b1607ec495d85d464499
Gabriel <bielrabelogatto@gmail.com>
2026-09-14 00:12:57 -0300

    Aprendendo sobre Bias

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,11 @@ int main() {
     torch::Tensor entrada = torch::tensor({10, 5});
     torch::Tensor peso = torch::tensor({0.5, 0.2});
 
+    torch::Tensor bias = torch::tensor({1.0});
+
     torch::Tensor resultado = entrada * peso;
 
-    torch::Tensor saida = resultado.sum();
+    torch::Tensor saida = resultado.sum() + bias;
 
     cout << saida << endl;
 
