1b8b06bea3c1fc9896947cb43aaa7eaad70d3d4c
Gabriel <bielrabelogatto@gmail.com>
2026-09-19 00:22:21 -0300

    Aprendendo a fazer a IA saber o quanto errou ou se acertou

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,10 @@
 using namespace std;
 
 int main() {
-    torch::Tensor valor = torch::tensor({-5.0, 2.0, -1.0, 8.0});
+    torch::Tensor resposta = torch::tensor({7.0});
+    torch::Tensor certaResposta = torch::tensor({10.0});
 
-    torch::Tensor resultado = torch::relu(valor);
+    torch::Tensor resultado = torch::pow(resposta - certaResposta, 2);
 
     cout << resultado << endl;
 
